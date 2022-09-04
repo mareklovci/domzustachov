c0151a68ff76698816f54d9506ac2c565fa57f19
mareklovci <mareklovci@gmail.com>
2022-09-04 12:05:16 +0200

    feat: apply custom fonts

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,7 @@ import { PrismicPreview } from "@prismicio/next";
 import { repositoryName, linkResolver } from "../prismicio";
 
 import "../styles/globals.css";
+import "../styles/fonts.css";
 import { Heading } from "../components/Heading";
 
 const NextLinkShim = ({ href, children, locale, ...props }) => {
@@ -69,32 +70,6 @@ export default function App({ Component, pageProps }) {
       internalLinkComponent={NextLinkShim}
       richTextComponents={richTextComponents}
     >
-      {/* TODO: Remove the following element once you have read the documentation. */}
-      {process.env.NODE_ENV === "development" && (
-        <div
-          style={{
-            background: "#5163ba",
-            padding: "1rem",
-            textAlign: "center",
-            fontSize: "0.85rem",
-            color: "#fff",
-          }}
-        >
-          <p>
-            <strong>👋 Welcome to your new website!</strong> To customize the
-            code and content of this site,{" "}
-            <a
-              href="https://github.com/prismicio-community/nextjs-starter-prismic-blog/tree/master/docs"
-              target="_blank"
-              rel="noreferrer"
-              style={{ textDecoration: "underline" }}
-            >
-              see the documentation
-            </a>
-            . Remove this bar in <code>pages/_app.js</code>.
-          </p>
-        </div>
-      )}
       <PrismicPreview repositoryName={repositoryName}>
         <Component {...pageProps} />
       </PrismicPreview>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,22 @@
+const { fontFamily } = require('tailwindcss/defaultTheme');
+
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./pages/**/*.js", "./components/**/*.js", "./slices/**/*.js"],
+  content: ['./pages/**/*.js', './components/**/*.js', './slices/**/*.js'],
   theme: {
     fontFamily: {
-      sans: 'Inter, ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"',
+      sans: 'Argent, ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"',
       serif:
-        '"Libre Baskerville", ui-serif, Georgia, Cambria, "Times New Roman", Times, serif',
+        'Brandon, ui-serif, Georgia, Cambria, "Times New Roman", Times, serif',
     },
-    extend: {},
+    extend: {
+      fontFamily: {
+        primary: ['Argent', ...fontFamily.sans],
+        secondary: ['Brandon', ...fontFamily.serif],
+        argent: ['Argent', ...fontFamily.sans],
+        brandon: ['Brandon', ...fontFamily.serif],
+      },
+    },
   },
-  plugins: [require("@tailwindcss/aspect-ratio")],
+  plugins: [require('@tailwindcss/aspect-ratio')],
 };
